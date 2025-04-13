c065fcdbfcb25b7fb7d588b6cc9c29f6fea8eb8b
Michael Vogtherr <vogtherr.michael@gmail.com>
2025-04-13 17:36:20 +0200

    git remote add origin

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  let base = '/'
+
+  if (mode === 'test') {
+    base = '/investment-calculator/'
+  }
+
+  return {
+    plugins: [react()],
+    base,
+  }
 })
